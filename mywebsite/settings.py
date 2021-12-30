21d9e0f0afe6b54997a14ef2fca57cefc658fa9a
Rajatgit6 <66821886+Rajatgit6@users.noreply.github.com>
2021-12-30 18:51:22 +0530

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR,"static",]
-MEDIA_ROOT=BASE_DIR/"media"
-MEDIA_URL='/media/'
+    os.path.join(BASE_DIR,  "static"),
+]
 # Default priary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
