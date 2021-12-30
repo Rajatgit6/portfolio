b0b31ff0b1ca7b76d486b6d2c0af25402a7d6794
Rajatgit6 <66821886+Rajatgit6@users.noreply.github.com>
2021-12-31 01:53:09 +0530

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -119,11 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/"static"
-MEDIA_URL = '/media/'
-MEDIA_ROOT=BASE_DIR/"media"
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+#MEDIA_ROOT=BASE_DIR/"media"
+MEDIA_URL='/media/'
 
 
 # Default priary key field type
